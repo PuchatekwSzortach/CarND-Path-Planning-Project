958b32cc6be22047374cb115aabe122cea9138d7
Kolodziejczyk Jakub <k.jakub@lexues.co.jp>
2017-07-25 13:18:29 +0900

    Reworked safety cost to take into account cars behind us in lane we want to go to

--- src/cost.h
+++ src/cost.h
@@ -41,26 +41,20 @@ class CostComputer
 
     int get_lowest_cost_trajectory_index(vector<Trajectory> &trajectories)
     {
-        std::cout << "\n\n" << std::endl ;
         vector<double> costs ;
 
         for(int index = 0 ; index < trajectories.size() ; ++index)
         {
-            trajectories[index].print() ;
+//            trajectories[index].print() ;
             double cost = 0 ;
 
             cost += 100.0 * this->get_target_speed_cost(trajectories[index]) ;
-
-            double safety_cost = this->get_safety_cost_two(trajectories[index]) ;
-            std:cout << "Safety cost is " << safety_cost << std::endl ;
-            cost += safety_cost ;
-
+            cost += this->get_safety_cost_two(trajectories[index]) ;
             cost += this->get_lane_change_cost(trajectories[index]) ;
 
             costs.push_back(cost) ;
 
         }
-
         return get_arg_min(costs) ;
     }
 
@@ -136,26 +130,24 @@ class CostComputer
 
             double safety_s_distance = 20.0 ;
 
-            if(ego_initial_s[0] < vehicle_s && vehicle_s < ego_final_s[0])
+            // If vehicle is in lane we want to be at the end of the trajectory (simplistic)
+            if(std::abs(vehicle_d - ego_final_d[0]) < 1.5)
             {
+                bool are_we_keeping_lane = ((std::abs(ego_initial_d[0] - ego_final_d[0])) < 1.0) ;
 
-                if(ego_d_left < vehicle_d_left && vehicle_d_right < ego_d_right)
+                // If we are not changing lanes, only look at s ahead of us
+                // Else look at s some distance behind us as well
+                double start_s = are_we_keeping_lane ? ego_initial_s[0] : ego_initial_s[0] - (ego_initial_s[1] * 1.0) ;
+
+                // If vehicle is between where we are now and will be at the end of trajectory
+                if(start_s < vehicle_s && vehicle_s < ego_final_s[0])
                 {
-                    std::cout << "\tVehicle " << vehicle_id << " is " << vehicle_s - ego_initial_s[0] << " metres in front of us" << std::endl ;
-                    std::cout << "\tIts speed is " << vehicle_sd_speed[0] << " and " << vehicle_sd_speed[1] << std::endl ;
-                    std::cout << "\tOur speed is " << ego_initial_s[1] << " and " << ego_initial_d[1] << std::endl ;
-                    std::cout << "\tTime horizon is: " << this->configuration.trajectory_time << std::endl ;
-                    std::cout << "\tOur final s is " << ego_final_s[0] + safety_s_distance << ", its " << vehicle_final_s << std::endl ;
-
+                    // And we risk passing vehicle at the end of trajectory
                     if(vehicle_final_s < ego_final_s[0] + safety_s_distance)
                     {
-                        std::cout << "\tIt is in our s range" << std::endl ;
-                        std::cout << "\tAnd it is in our band too" << std::endl ;
                         double difference = ego_final_s[0] + safety_s_distance - vehicle_final_s ;
                         cost += difference * difference ;
-
                     }
-
                 }
             }
 
